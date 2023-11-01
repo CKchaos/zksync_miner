a1b5e13981c73b4da6f0ac7e03dcd4004a25c202
CKchaos <rongtian.ye@aalto.fi>
2023-11-01 20:51:11 +0800

    print number of acc with >100 tx

--- task_deployer.py
+++ task_deployer.py
@@ -316,6 +316,7 @@ class TaskDeployer():
             logging.info('Getting candidate nonces ...')
             nonces = self.get_candidate_nonces(task_candidates)
             logging.info('Number of all transactions: %d' % sum(nonces))
+            logging.info('Number of accounts with >100 transactions: %d' % sum(np.array(nonces) > 100))
 
             logging.info('Getting non-active times ...')
             non_active_times = self.get_non_active_times(task_candidates)

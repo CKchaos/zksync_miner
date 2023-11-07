262213d73126ceb81a1e66f2a82069a756b866b0
CKchaos <rongtian.ye@aalto.fi>
2023-11-07 20:01:38 +0800

    update task acc sampling strategy

--- task_deployer.py
+++ task_deployer.py
@@ -146,12 +146,11 @@ class TaskDeployer():
         acc_num = len(candidates)
 
         prob = np.ones(acc_num) * 2
-        prob[nonces > 100] = 0
-        prob[non_active_times > 86400 * 2] = 0.6
+        prob[nonces >= 100] = 0
+        prob[non_active_times > 86400 * 2] = 0.5
         prob[non_active_times > 86400 * 3] = 3
         prob[nonces < 80] = 6
         prob[non_active_times > 86400 * 4] = 25
-        prob[non_active_times < 3600 * 12] = 0
         prob = prob / np.sum(prob)
 
         non_zero_prob = np.sum(prob > 0)

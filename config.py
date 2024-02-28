e0e4e7402ccb7995ce3bf4faa155c13b8ed9a63b
CKchaos <rongtian.ye@aalto.fi>
2024-02-28 12:42:38 +0800

    update dapp list

--- config.py
+++ config.py
@@ -249,7 +249,7 @@ SWAP_TRADABLE_TOKENS = {
     #'iZumi': ZK_IZUMI_TOKENS,
     'Maverick': ZK_MAVERICK_TOKENS,
     'Odos': ZK_ODOS_TOKENS,
-    'XYSwap': ZK_XYSWAP_TOKENS,
+    #'XYSwap': ZK_XYSWAP_TOKENS,
     'WooFi': ZK_WOOFI_TOKENS,
 }
 
